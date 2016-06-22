544c20e25c8542f42b120f01ee10418788429936
Vegard Øye <vegard_oye@hotmail.com>
2016-06-22 09:27:39 +0200

    CSS transitions
    This makes the UI elements less intrusive.

--- js/custom.js
+++ js/custom.js
@@ -79,7 +79,6 @@ function smartquotes () {
       // .replace(/((\u2018[^']*)|[a-z])'([^0-9]|$)/ig, '$1\u2019$3') // conjunction's possession
       // .replace(/(\u2018)([0-9]{2}[^\u2019]*)(\u2018([^0-9]|$)|$|\u2019[a-z])/ig, '\u2019$2$3') // abbrev. years like '93
       // .replace(/(\B|^)\u2018(?=([^\u2019]*\u2019\b)*([^\u2019\u2018]*\W[\u2019\u2018]\b|[^\u2019\u2018]*$))/ig, '$1\u2019') // backwards apostrophe
-
         .replace(/<-+>/g, '\u2194') // double arrow
         .replace(/<=+>/g, '\u21D4')
         .replace(/-+>/g, '\u2192') // right arrow
