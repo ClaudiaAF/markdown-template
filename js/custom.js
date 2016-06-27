1c72824701ea38a460d2fcc93284c82ca7375101
Vegard Øye <vegard_oye@hotmail.com>
2016-06-27 13:44:34 +0200

    Add footnotes

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,13 @@
 /* global $ */
 /* jshint asi:true */
 
+var hljs = window.hljs
 var attr = window.markdownItAttrs
 var anchor = window.markdownItAnchor
 var sub = window.markdownitSub
 var sup = window.markdownitSup
-var hljs = window.hljs
+var footnote = window.markdownitFootnote
+
 var md = window.markdownit({
   html: true,
   typographer: true,
@@ -18,26 +20,11 @@ var md = window.markdownit({
 
     return '' // use external default escaping
   }
-})
-
-// // Actual default values
-// var md = require('markdown-it')({
-//   highlight: function (str, lang) {
-//     if (lang && hljs.getLanguage(lang)) {
-//       try {
-//         return hljs.highlight(lang, str).value;
-//       } catch (__) {}
-//     }
-
-//     return ''; // use external default escaping
-//   }
-// });
-
-
-md.use(attr)
-md.use(anchor, {permalink: true, permalinkBefore: true})
-md.use(sub)
-md.use(sup)
+}).use(attr)
+  .use(anchor, {permalink: true, permalinkBefore: true})
+  .use(sub)
+  .use(sup)
+  .use(footnote)
 
 function convert (markdown) {
   var html = md.render(markdown)
