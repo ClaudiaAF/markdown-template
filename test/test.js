d9b5e87ec293d247bd61f78c618a5161e6695836
Vegard Øye <vegard_oye@hotmail.com>
2016-07-05 10:00:29 +0200

    BDD style
    Replace assert() with .should().

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-/* global describe, it, $, assert */
+/* global describe, it, $ */
 describe('collapse.js', function () {
   describe('addSection()', function () {
     it('should create a section for a header', function () {
@@ -9,21 +9,21 @@ describe('collapse.js', function () {
                   '</div>')
       var header = div.find('h1')
       $.fn.addCollapsibleSections.addSection(header)
-      assert.equal(div.prop('outerHTML'),
-                   '<div>' +
-                   '<h1>Header</h1>' +
-                   '<div>' +
-                   '<p>Paragraph one</p>' +
-                   '<p>Paragraph two</p>' +
-                   '</div>' +
-                   '</div>')
+      div.prop('outerHTML').should.equal(
+        '<div>' +
+        '<h1>Header</h1>' +
+        '<div>' +
+        '<p>Paragraph one</p>' +
+        '<p>Paragraph two</p>' +
+        '</div>' +
+        '</div>')
     })
   })
 
   describe('button()', function () {
     it('should create a button as specified', function () {
-      assert.equal($.fn.addCollapsibleSections.button('\u25bc').prop('outerHTML'),
-                   '<span aria-hidden="true" class="collapse-button" title="Collapse">▼</span>')
+      $.fn.addCollapsibleSections.button('\u25bc').prop('outerHTML').should.equal(
+        '<span aria-hidden="true" class="collapse-button" title="Collapse">▼</span>')
     })
   })
 
@@ -37,23 +37,23 @@ describe('collapse.js', function () {
                   '<p>Paragraph three</p>' +
                   '<p>Paragraph four</p>' +
                   '</div>')
-      assert.equal(div.addCollapsibleSections().prop('outerHTML'),
-                   '<div>' +
-                   '<h1>Header' +
-                   '<span aria-hidden="true" class="collapse-button" title="Collapse">▼</span>' +
-                   '</h1>' +
-                   '<div>' +
-                   '<p>Paragraph one</p>' +
-                   '<p>Paragraph two</p>' +
-                   '<h2>Subheader' +
-                   '<span aria-hidden="true" class="collapse-button" title="Collapse">▼</span>' +
-                   '</h2>' +
-                   '<div>' +
-                   '<p>Paragraph three</p>' +
-                   '<p>Paragraph four</p>' +
-                   '</div>' +
-                   '</div>' +
-                   '</div>')
+      div.addCollapsibleSections().prop('outerHTML').should.equal(
+        '<div>' +
+        '<h1>Header' +
+        '<span aria-hidden="true" class="collapse-button" title="Collapse">▼</span>' +
+        '</h1>' +
+        '<div>' +
+        '<p>Paragraph one</p>' +
+        '<p>Paragraph two</p>' +
+        '<h2>Subheader' +
+        '<span aria-hidden="true" class="collapse-button" title="Collapse">▼</span>' +
+        '</h2>' +
+        '<div>' +
+        '<p>Paragraph three</p>' +
+        '<p>Paragraph four</p>' +
+        '</div>' +
+        '</div>' +
+        '</div>')
     })
   })
 })
@@ -66,21 +66,13 @@ describe('figure.js', function () {
                   '<img alt="Caption text" class="right" src="image.png" width="200">' +
                   '</p>' +
                   '</div>')
-      var html = div.addFigures().prop('outerHTML')
-      assert.isTrue(html ===
-                    '<div>' +
-                    '<div style="width: 209px;" class="figure right">' +
-                    '<img alt="Caption text" class="right" src="image.png" width="200">' +
-                    '<p class="caption">Caption text</p>' +
-                    '</div>' +
-                    '</div>' ||
-                    html ===
-                    '<div>' +
-                    '<div class="figure right" style="width: 209px; ">' +
-                    '<img alt="Caption text" class="right" src="image.png" width="200">' +
-                    '<p class="caption">Caption text</p>' +
-                    '</div>' +
-                    '</div>')
+      div.addFigures().prop('outerHTML').should.equal(
+        '<div>' +
+        '<div style="width: 209px;" class="figure right">' +
+        '<img alt="Caption text" class="right" src="image.png" width="200">' +
+        '<p class="caption">Caption text</p>' +
+        '</div>' +
+        '</div>')
     })
   })
 })
@@ -92,21 +84,21 @@ describe('punctuation.js', function () {
                   '<p>"Tsk, tsk," said the Hatter, "what a mess you\'ve made."</p>' +
                   '<p>"It is perfectly fine," replied Alice calmly. "I will leave it for the garbage collection service to recover."</p>' +
                   '</div>')
-      assert.equal(div.addPunctuation().prop('outerHTML'),
-                   '<div>' +
-                   '<p>“Tsk, tsk,” said the Hatter, “what a mess you’ve made.”</p>' +
-                   '<p>“It is perfectly fine,” replied Alice calmly. “I will leave it for the garbage collection service to recover.”</p>' +
-                   '</div>')
+      div.addPunctuation().prop('outerHTML').should.equal(
+        '<div>' +
+        '<p>“Tsk, tsk,” said the Hatter, “what a mess you’ve made.”</p>' +
+        '<p>“It is perfectly fine,” replied Alice calmly. “I will leave it for the garbage collection service to recover.”</p>' +
+        '</div>')
     })
 
     it('should add horizontal ellipsis', function () {
       var div = $('<div>' +
                   '<p>Is that so...?</p>' +
                   '</div>')
-      assert.equal(div.addPunctuation().prop('outerHTML'),
-                   '<div>' +
-                   '<p>Is that so…?</p>' +
-                   '</div>')
+      div.addPunctuation().prop('outerHTML').should.equal(
+        '<div>' +
+        '<p>Is that so…?</p>' +
+        '</div>')
     })
   })
 })
@@ -115,14 +107,14 @@ describe('hanging.js', function () {
   describe('addHangingPunctuation()', function () {
     it('should detect opening double quotation marks', function () {
       var div = $('<div><p>“Tsk, tsk,” said the Hatter, “what a mess you’ve made.”</p></div>')
-      assert.equal(div.addHangingPunctuation().prop('outerHTML'),
-                   '<div><p class="startsWithDoubleQuote">“Tsk, tsk,” said the Hatter, “what a mess you’ve made.”</p></div>')
+      div.addHangingPunctuation().prop('outerHTML').should.equal(
+        '<div><p class="startsWithDoubleQuote">“Tsk, tsk,” said the Hatter, “what a mess you’ve made.”</p></div>')
     })
 
     it('should detect opening single quotation marks', function () {
       var div = $('<div><p>‘Tsk, tsk,’ said the Hatter, ‘what a mess you’ve made.’</p></div>')
-      assert.equal(div.addHangingPunctuation().prop('outerHTML'),
-                   '<div><p class="startsWithSingleQuote">‘Tsk, tsk,’ said the Hatter, ‘what a mess you’ve made.’</p></div>')
+      div.addHangingPunctuation().prop('outerHTML').should.equal(
+        '<div><p class="startsWithSingleQuote">‘Tsk, tsk,’ said the Hatter, ‘what a mess you’ve made.’</p></div>')
     })
   })
 })
@@ -131,14 +123,14 @@ describe('title.js', function () {
   describe('removeAria()', function () {
     it('should remove header anchor', function () {
       var header = $('<h1><a aria-hidden="true" href="#">¶</a>Header</h1>')
-      assert.equal(header.removeAria().text(),
-                   'Header')
+      header.removeAria().text().should.equal(
+        'Header')
     })
 
     it('should remove collapse button', function () {
       var header = $('<h1>Header<span title="Collapse" class="collapse-button" aria-hidden="true">▲</span></h1>')
-      assert.equal(header.removeAria().text(),
-                   'Header')
+      header.removeAria().text().should.equal(
+        'Header')
     })
   })
 
@@ -153,12 +145,12 @@ describe('title.js', function () {
                   '<p>Paragraph</p>' +
                   '</body>' +
                   '</div>')
-      assert.equal(div.addTitle().prop('outerHTML'),
-                   '<div>' +
-                   '<title>Header</title>' +
-                   '<h1>Header</h1>' +
-                   '<p>Paragraph</p>' +
-                   '</div>')
+      div.addTitle().prop('outerHTML').should.equal(
+        '<div>' +
+        '<title>Header</title>' +
+        '<h1>Header</h1>' +
+        '<p>Paragraph</p>' +
+        '</div>')
     })
   })
 })
